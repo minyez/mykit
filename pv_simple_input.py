f5a3615dd30d36a36fff50de13da0f3fc26f15aa
minyez <stevezhang@pku.edu.cn>
2018-03-26 20:28:23 +0800

    kpath generator

--- pv_simple_input.py
+++ pv_simple_input.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
 
-from pv_calc_utils import *
+from __future__ import print_function
+from pv_calc_utils import vasp_io_get_NPAR, \
+                          vasp_write_kpoints_basic, \
+                          vasp_write_incar_minimal_elec
 from pv_classes import vasp_read_poscar
+from argparse import ArgumentParser
 import os
+import sys
 
 # =====================================================
 
@@ -15,7 +20,7 @@ def Main(ArgList):
 # =================== Parser ==========================
 
     parser = ArgumentParser(description=description)
-    parser.add_argument("-i",dest='posin',default="POSCAR",help="Input file for KPOINTS generation. POSCAR as default")
+    parser.add_argument("-i",dest='poscar',default="POSCAR",help="Input file for KPOINTS generation. POSCAR as default")
     parser.add_argument("-e",dest='encut',type=int,default=0,help="Planewave cutoff. 0 will set largest ENMAX")
     parser.add_argument("-n",dest='nproc',type=int,default=1,help="Number of processors ")
     parser.add_argument("-x",dest='tag_xc',default=None,help="type of XC functional for input orbitals, \
@@ -24,7 +29,7 @@ def Main(ArgList):
     parser.add_argument("--spin",dest='ispin',type=int,default=1,help="Spin-polarization. 1 for nsp and 2 for sp.")
 
     opts  = parser.parse_args()
-    posin = opts.posin
+    poscar = opts.poscar
     klen  = opts.klen
     tag_xc= opts.tag_xc
     ispin = opts.ispin
@@ -32,24 +37,24 @@ def Main(ArgList):
     encut = opts.encut
     npar  = vasp_io_get_NPAR(nproc)
 
-    print " ============ pv_simple_input.py ============"
+    print(" ============ pv_simple_input.py ============")
 
-    if os.path.exists(posin) and (klen>=0):
-        print " Writing KPOINTS...(check odd/even yourself!)"
-        poscar = vasp_read_poscar(posin)
+    if os.path.exists(poscar) and (klen>=0):
+        print(" Writing KPOINTS...(check odd/even yourself!)")
+        poscar = vasp_read_poscar(poscar)
         if klen==0:
-            print "  - Warning: KLEN is not specified. Use default value: 30"
-            print "  - Warning: you need to specify it yourself for metallic system"
+            print("  - Warning: KLEN is not specified. Use default value: 30")
+            print("  - Warning: you need to specify it yourself for metallic system")
             klen = 30
         nks = [int(klen/x) for x in poscar.lenlat]
         vasp_write_kpoints_basic(nks,'G')
 
-    elif not os.path.exists(posin):
-        print " - Error: need POSCAR file to generate KPOINTS. Exit"
+    elif not os.path.exists(poscar):
+        print(" - Error: need POSCAR file to generate KPOINTS. Exit")
         sys.exit(1)
 
     with open('INCAR','w') as incar:
-        print " Writing INCAR..."
+        print(" Writing INCAR...")
         vasp_write_incar_minimal_elec(incar,tag_xc,encut=encut,npar=npar,spin=ispin)
 
 # =====================================================
---
+++ pw_kpath_gen.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+# ====================================================
+#
+#     File Name : pw_kpath_gen.py
+# Creation Date : 21-03-2018
+#    Created By : Min-Ye Zhang
+#       Contact : stevezhang@pku.edu.cn
+#
+# ====================================================
+
+from __future__ import print_function
+import sys
+
+def pw_kpath_gen(ArgList):
+    '''
+    Generate the case.klist_band file for the band structure calculation by WIEN2K
+
+    Mandatory input:
+    @SpecKpt: the name of file that includes the special points appearing in order along the path
+    @grid: the number of points between two special points
+    '''
+    pass
+
+
+# ==============================
+
+if __name__ == "__main__":
+    pw_kpath_gen(sys.argv)
+
