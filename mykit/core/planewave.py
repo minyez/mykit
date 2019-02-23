788f7c3a97007716f7963a7fd163b5d743c88e06
minyez <stevezhang@pku.edu.cn>
2019-02-23 21:45:08 +0800

    bugfix map_tags for single tag

--- mykit/core/planewave.py
+++ mykit/core/planewave.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 '''define classes and functions related to plane wave basis setup
 '''
+from mykit.core.log import verbose
+
 class planewaveError(Exception):
     pass
 
 
-class plane_wave:
+class plane_wave(verbose):
     '''the base class that manage parameters of plane-wave basis.
     '''
 
@@ -25,35 +27,32 @@ class plane_wave:
 
     def __init__(self, **pwargs):
         if len(pwargs) > 0:
-            self.__initialize_pwtags(**pwargs)
+            self.__parse_pwtags(**pwargs)
         else:
             pass
 
-    # def __getitem__(self, index):
-    #     return self.__pwTags[index]
-    
-    def __initialize_pwtags(self, **kwargs):
-        '''initialize the fundamental tags of pw class
+    # def __initialize_pwtags(self, **kwargs):
+    #     '''initialize the fundamental tags of pw class
 
-        This will purge out all non-available tags
-        '''
-        for _pwt in kwargs:
-            self.__parse_pwtag(_pwt, kwargs[_pwt])
+    #     This will purge out all non-available tags
+    #     '''
+    #     for _pwt in kwargs:
+    #         self.__parse_pwtags(_pwt, kwargs[_pwt])
 
-    def parse_tag(self, tagName, value):
-        self.__parse_pwtag(tagName, value)
+    def parse_tags(self, **keyval):
+        self.__parse_pwtags(**keyval)
 
-    def __parse_pwtag(self, pwTagName, value):
-        if pwTagName in self.__pwTagMaps:
-            self.__pwTags.update({pwTagName: value})
+    def __parse_pwtags(self, **keyval):
+        if len(keyval) == 0:
+            return
+        for _k, _v in keyval.items():
+            if _k in self.__pwTagMaps:
+                self.__pwTags.update({_k:_v})
+        # if pwTagName in self.__pwTagMaps:
+            # self.__pwTags.update({pwTagName: value})
 
-    def _get_one_pwtag(self, pwTagName):
+    def __get_one_pwtag(self, pwTagName):
         return self.__pwTags.get(pwTagName, None)
-
-
-    @property
-    def pwTagvals(self):
-        return self.__pwTags
 
     def tag_vals(self, *tags):
         return self.__tag_vals(*tags, progName="n a")
@@ -66,10 +65,19 @@ class plane_wave:
         Returns:
             dict, with key-value pair as : ``program-specific tag: tag value``
         '''
+        if len(tags) == 0:
+            return None
         _vals = []
-        _pwtags = plane_wave.map2PwTags(*tags, progName=progName)
-        _pwvals = list(map(self._get_one_pwtag, _pwtags))
-        return _pwvals
+        # _pwtags = plane_wave.map2pwtags(*tags, progFrom=progName)
+        _pwtags = plane_wave.map_tags(*tags, progFrom=progName, progTo="n a")
+        self.print_log("Extracting plane_wave tags: ", _pwtags, level=3, depth=1)
+        if len(tags) == 1:
+            return self.__get_one_pwtag(_pwtags)
+        return list(map(self.__get_one_pwtag, _pwtags))
+
+    @property
+    def pwTagvals(self):
+        return self.__pwTags
 
     @classmethod
     def map_tags(cls, *tags, progFrom="n a", progTo="n a"):
@@ -80,23 +88,24 @@ class plane_wave:
             progFrom (str)
             progTo (str)
         '''
+        # ! Problem in get value of single tag 
         _pF = progFrom.lower()
         _pT = progTo.lower()
         _d = {}
-        
-        for _pwt in cls.__pwTagMaps:
-            _d.update({cls.__pwTagMaps.get(_pwt, {}).get(_pF, None): \
-                       cls.__pwTagMaps.get(_pwt, {}).get(_pT, None)})
+        for _pwt, _map in cls.__pwTagMaps.items():
+            # _d.update({cls.__pwTagMaps.get(_pwt, {}).get(_pF, None): \
+            #            cls.__pwTagMaps.get(_pwt, {}).get(_pT, None)})
+            _d.update({_map.get(_pF, None): _map.get(_pT, None)})
         # ensure tags not implemented will be mapped to None
         _d.update({None:None})
         if len(tags) == 0:
             return tuple(_d.values())
         if len(tags) == 1:
-            return _d.get(tags, None)
+            return _d.get(tags[0], None)
         return tuple(_d.get(_t, None) for _t in tags)
 
     @classmethod
-    def mapFromPwTags(cls, *pwTagNames, progName="n a"):
+    def map_from_pwtags(cls, *pwTagNames, progTo="n a"):
         
         '''Class method to map ``plane_wave`` tags to program-specific tags 
 
@@ -110,10 +119,10 @@ class plane_wave:
             a dict value, or a list of dict value corresponding to tagNames
             If no tagNames is specified, all available program-specifi tags will be returned.
         '''
-        return cls.map_tags(*pwTagNames, progTo=progName)
+        return cls.map_tags(*pwTagNames, progTo=progTo)
 
     @classmethod
-    def map2PwTags(cls, *tagNames, progName="n a"):
+    def map2pwtags(cls, *tagNames, progFrom="n a"):
         '''Class method to map program-specific tags to ``plane_wave`` tags
 
         If the tag name is not found, None will be return
@@ -126,4 +135,4 @@ class plane_wave:
             a dict value, or a list of dict value corresponding to tagNames
             If no tagNames is specified, all available plane_wave tags will be returned.
         '''
-        return cls.map_tags(*tagNames, progFrom=progName)
+        return cls.map_tags(*tagNames, progFrom=progFrom)
--- test/core/planewave_test.py
+++ test/core/planewave_test.py
@@ -12,8 +12,10 @@ haveImpleAbiMap = (None, None, None)
 
 class test_pwtags(ut.TestCase):
 
+    pass
+
     def test_implemented_pwtags(self):
-        implePwTags = plane_wave.map2PwTags()
+        implePwTags = plane_wave.map2pwtags()
         for _t in haveImplePwTags:
             self.assertIn(_t, implePwTags)
 
@@ -22,10 +24,16 @@ class test_tag_mapping(ut.TestCase):
     def test_map_tags(self):
         '''Test the map_tags classmethod
         '''
-        self.assertTupleEqual(haveImplePwTags, plane_wave.map_tags(*haveImplePwTags))
-        # Map plane_wave tags to tags for particular program. 
-        self.assertTupleEqual(haveImpleQeMap, plane_wave.map_tags(*haveImplePwTags, progTo="qe"))
-        self.assertTupleEqual(haveImpleVaspMap, plane_wave.map_tags(*haveImplePwTags, progTo="vasp"))
+        for _i,_v in enumerate(haveImplePwTags):
+            # single tag
+            self.assertEqual(haveImplePwTags[_i], plane_wave.map_tags(haveImplePwTags[_i]))
+            self.assertEqual(haveImpleVaspMap[_i], plane_wave.map_tags(haveImplePwTags[_i], progTo="vasp"))
+            self.assertEqual(haveImpleQeMap[_i], plane_wave.map_tags(haveImplePwTags[_i], progTo="qe"))
+            # multiple tags
+            if _i != 0:
+                self.assertTupleEqual(haveImplePwTags[:_i+1], plane_wave.map_tags(*haveImplePwTags[:_i+1]))
+                self.assertTupleEqual(haveImpleVaspMap[:_i+1], plane_wave.map_tags(*haveImplePwTags[:_i+1], progTo="vasp"))
+                self.assertTupleEqual(haveImpleQeMap[:_i+1], plane_wave.map_tags(*haveImplePwTags[:_i+1], progTo="qe"))
 
         # Map to plane_wave tags from tags for particular program. 
         # Should be noticed that the tag list to map from should not have None in it, 
@@ -37,12 +45,33 @@ class test_tag_mapping(ut.TestCase):
         # Should be noticed that the tag list to map from should not have None in it, 
         # otherwise the following test must fail.
         self.assertTupleEqual(haveImpleQeMap, plane_wave.map_tags(*haveImpleVaspMap, progFrom="vasp", progTo="qe"))
-
-class test_intialize(ut.TestCase):
     
-    def test_direct_init(self):
+    def test_map2pwtags(self):
+        '''Test the map2pwtags classmethod
+        '''
+        self.assertTupleEqual(haveImplePwTags, plane_wave.map2pwtags(*haveImplePwTags, progFrom="n a"))
+        
+    def test_map_from_pwtags(self):
+        '''Test the map_from_pwtags classmethod
+        '''
+        self.assertTupleEqual(haveImpleVaspMap, plane_wave.map_from_pwtags(*haveImplePwTags, progTo="vasp"))
+        
+
+class test_tag_manipulation(ut.TestCase):
+
+    pass
+    
+    def test_initialization(self):
+        # empty initialization
+        _pw = plane_wave()
         _pw = plane_wave(encutPw=300, restartWave=0)
         self.assertListEqual([300, 0], _pw.tag_vals("encutPw", "restartWave"))
+
+    def test_parse_tags(self):
+        _pw = plane_wave(restartWave=0)
+        self.assertEqual(0, _pw.tag_vals("restartWave"))
+        _pw.parse_tags(encutPw=200)
+        self.assertEqual(200, _pw.tag_vals("encutPw"))
         
 
 if __name__ == "__main__":
