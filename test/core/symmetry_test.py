e17947c1682afbeba4320ab0d5feef773a0d091e
minyez <stevezhang@pku.edu.cn>
2019-03-12 21:15:29 +0800

    preliminary unit test _spk

--- test/core/symmetry_test.py
+++ test/core/symmetry_test.py
@@ -3,6 +3,10 @@
 
 import unittest as ut
 
+from mykit.core.metadata._spk import (_special_kpoints, cond_a_lt_b,
+                                      cond_abc_invsq, cond_any, cond_c_lt_a,
+                                      cond_curt_a_lt_sqrt_c,
+                                      cond_max_bc_noneq_left)
 from mykit.core.symmetry import (Symmetry, SymmetryError, space_group,
                                  special_kpoints)
 
@@ -24,7 +28,64 @@ class test_space_group(ut.TestCase):
             space_group.get_spg_symbol, 231)
         self.assertRaisesRegex(SymmetryError, r"Invalid space group id \(1~230\): -1", \
             space_group.get_spg_symbol, -1)
-        
+
+class test_spk_dict(ut.TestCase):
+    '''Test the integrity of _special_kpoints dictionary
+    '''
+    def test_cond_func(self):
+        self.assertEqual(cond_any(1,2,3), 0)
+
+        self.assertEqual(cond_a_lt_b(2,1,3), 0)
+        self.assertEqual(cond_a_lt_b(1,2,3), 1)
+        self.assertRaisesRegex(ValueError, r"Fail to determine special kpoints set", cond_a_lt_b, 1, 1, 3)
+
+        self.assertEqual(cond_c_lt_a(2,1,3), 0)
+        self.assertEqual(cond_c_lt_a(3,1,2), 1)
+        self.assertRaisesRegex(ValueError, r"Fail to determine special kpoints set", cond_c_lt_a, 1, 3, 1)
+
+        self.assertEqual(cond_curt_a_lt_sqrt_c(8,1,3), 0)
+        self.assertEqual(cond_curt_a_lt_sqrt_c(8,1,6), 1)
+        self.assertRaisesRegex(ValueError, r"Fail to determine special kpoints set", cond_curt_a_lt_sqrt_c, 8, 1, 4)
+
+        self.assertEqual(cond_max_bc_noneq_left(2,5,3), 0)
+        self.assertEqual(cond_max_bc_noneq_left(3,5,2), 0)
+        self.assertEqual(cond_max_bc_noneq_left(2,3,5), 1)
+        self.assertEqual(cond_max_bc_noneq_left(3,2,5), 1)
+        self.assertRaisesRegex(ValueError, r"Fail to determine special kpoints set", cond_max_bc_noneq_left, 2, 5, 2)
+        self.assertRaisesRegex(ValueError, r"Fail to determine special kpoints set", cond_max_bc_noneq_left, 2, 2, 5)
+        self.assertRaisesRegex(ValueError, r"Fail to determine special kpoints set", cond_max_bc_noneq_left, 5, 2, 2)
+
+        self.assertEqual(cond_abc_invsq(0.1,0.5,0.5), 0)
+        self.assertEqual(cond_abc_invsq(0.5,0.5,0.1), 1)
+        self.assertEqual(cond_abc_invsq(0.2,0.2,0.5), 2)
+        self.assertRaisesRegex(ValueError, r"Fail to determine special kpoints set", cond_abc_invsq, 0.5, 0.1, 0.5)
+
+
+    def test_number_of_spg(self):
+        for i in range(1, 231):
+            self.assertIn(i, _special_kpoints.keys())
+        self.assertEqual(230, len(_special_kpoints.keys()))
+
+    def test_has_cond_and_spPrim(self):
+        for k, v in _special_kpoints.items():
+            self.assertIn("cond", v, msg="bad key {}".format(k))
+            self.assertIn("spPrim", v, msg="bad key {}".format(k))
+            self.assertTrue(isinstance(v["spPrim"], list), msg="bad key {}".format(k))
+
+    def test_number_of_sets(self):
+        for k, v in _special_kpoints.items():
+            if v["cond"] == cond_any:
+                self.assertEqual(1, len(v["spPrim"]), msg="bad key {}".format(k))
+            if v["cond"] == cond_abc_invsq:
+                self.assertEqual(3, len(v["spPrim"]), msg="bad key {}".format(k))
+            hasTwoSets = [
+                cond_a_lt_b, 
+                cond_c_lt_a, 
+                cond_curt_a_lt_sqrt_c,
+                cond_max_bc_noneq_left,
+                ]
+            if v["cond"] in hasTwoSets:
+                self.assertEqual(2, len(v["spPrim"]), msg="bad key {}".format(k))
 
 
 if __name__ == '__main__':
